20078016fe5356b201225c175e593030b03eba80
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-12-04 10:44:59 +0530

    completed login and register pages
    Co-Authored-By: Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import Navbar from '../Navbar/Navbar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 const Login = () => {
     const [email, setEmail] = useState("");
@@ -48,6 +49,10 @@ const Login = () => {
     }
   return (
     <>
+        <Helmet>
+            <title>DevCode | Login</title>
+            <meta name="description" content="Login to your Devcode account here." />
+        </Helmet>
         <section className="bg-black">
             <div className="nav">
                 <Navbar />
@@ -64,11 +69,11 @@ const Login = () => {
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={login}>
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
                                 <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className=" border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" required="" />
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
                                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required="" />
                             </div>
                             <div className="flex items-center justify-between">
@@ -84,7 +89,7 @@ const Login = () => {
                             </div>
                             <button type="submit" className="bg-grey-200 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-400">
-                                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline text-primary-500">Sign up</a>
+                                Don't have an account yet? <a href="/register" className="font-medium text-primary-600 hover:underline text-primary-500">Sign up</a>
                             </p>
                         </form>
                     </div>
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -5,6 +5,7 @@ import Navbar from '../Navbar/Navbar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 
 const Register = () => {
     const [fName, setFName] = useState("");
@@ -49,51 +50,44 @@ const Register = () => {
     }
   return (
     <>
-        <section className="bg-black dark:bg-black">
+        <Helmet>
+            <title>DevCode | Register</title>
+            <meta name="description" content="Register for a new Devcode account here." />
+        </Helmet>
+        <section className="bg-black">
             <div className="nav">
                 <Navbar />
             </div>
             <div className="flex mt-[-90px] flex-col bg-black items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white">
                     <img className="w-32 h-32 mr-2" src={logo} alt="logo" />
                     {/* DevCode */}
                 </a>
-                <div className="w-full bg-gray-800 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
+                        <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                            Register to your account
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={register}>
                             <div>
-                                <label htmlFor="fname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                                <input value={fName} onChange={(e) => setFName(e.target.value)} type="text" name="fname" id="fname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Elon" required="" />
+                                <label htmlFor="fname" className="block mb-2 text-sm font-medium text-white">First Name</label>
+                                <input value={fName} onChange={(e) => setFName(e.target.value)} type="text" name="fname" id="fname" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Elon" required="" />
                             </div>
                             <div>
-                                <label htmlFor="lname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                                <input value={lName} onChange={(e) => setlName(e.target.value)} type="text" name="lname" id="lname" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Musk" required="" />
+                                <label htmlFor="lname" className="block mb-2 text-sm font-medium text-white">Last Name</label>
+                                <input value={lName} onChange={(e) => setlName(e.target.value)} type="text" name="lname" id="lname" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Musk" required="" />
                             </div>
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" required="" />
                             </div>
                             <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required="" />
                             </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            <button type="submit" className="w-full text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800">Register</button>
+                            <p className="text-sm font-light text-gray-400">
+                                Already Have an account? <a href="/login" className="font-medium hover:underline text-primary-500">Sign In</a>
                             </p>
                         </form>
                     </div>
