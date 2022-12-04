fdc9b17a58ef26c696bc88f7f5f780b786d80e14
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-12-05 00:43:15 +0530

    Adding Collaborate Button
    Co-authored-by: devarshishimpi <devarshishimpi@gmail.com>

--- frontend/src/components/Projects/Projects.jsx
+++ frontend/src/components/Projects/Projects.jsx
@@ -46,7 +46,7 @@ const Projects = () => {
             <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
                 {projectList.map((project) => {
                     return (
-                        <div key={project._id} className="overflow-hidden shadow-lg bg-[#262626] p-0 rounded-xl">
+                        <div key={project._id} className="relative overflow-hidden shadow-lg bg-[#262626] p-0 rounded-xl">
                             {project.level === 1 && <span class="absolute font-extrabold bg-green-100 text-green-800 text-lg mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900">Beginner</span>}
                             {project.level === 2 && <span class="absolute font-extrabold bg-yellow-100 text-yellow-800 text-lg mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900">Intermediate</span>}
                             {project.level === 3 && <span class="absolute font-extrabold bg-purple-100 text-purple-800 text-lg mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">Advance</span>}
@@ -58,7 +58,7 @@ const Projects = () => {
                                 {project.description}
                                 </p>
                             </div>
-                            <div className="px-6 pt-4 pb-2">
+                            <div className="px-6 pt-4 pb-2 mb-[55px]">
                                 {project.languages.map((language, index) => {
                                     return (
                                         <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">#{language}</span>
@@ -67,6 +67,7 @@ const Projects = () => {
                                 {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">#travel</span>
                                 <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">#winter</span> */}
                             </div>
+                            <a href={project.repoLink} target="_blank" className='bg-purple-500 text-white bottom-0 p-4 flex w-full justify-center items-center absolute'>Collaborate</a>
                         </div>
                     );
                 })}
