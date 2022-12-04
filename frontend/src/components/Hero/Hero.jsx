abb8b69b7f74058408e4bc82013798bd3e6294e1
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-12-04 16:30:07 +0530

    Update Hero.jsx
    Co-Authored-By: Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
             </form>
 
           </div>
-          <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+          {/*<div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
               <span className="font-semibold text-gray-400 uppercase">FEATURED IN</span>
               <div className="flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
                   <a href="#" className="mr-5 mb-5 lg:mb-0 hover:text-gray-400">
@@ -57,7 +57,7 @@ const Hero = () => {
                       </svg>                                                   
                   </a>         
               </div>
-          </div> 
+        </div>*/}
       </div>
     </section>
   )
