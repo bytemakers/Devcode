67d0b6bdf8ad1b7a9ea133702603ea96966faf50
Devarshi Shimpi <devarshishimpi@gmail.com>
2022-12-06 18:17:15 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = (props) => {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src="https://res.cloudinary.com/dvstech/image/upload/v1670330775/devcode/contact.png"
                         alt=""
                       />
                     </Menu.Button>
